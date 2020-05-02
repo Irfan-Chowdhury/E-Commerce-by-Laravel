7bbcd23a196a3fcfe931ddd10ed5123be28b2506
Irfan-Chowdhury <irfanchowdhury80@gmail.com>
2020-05-03 03:34:57 +0600

    Model & Migration setup of category,subcategory & brand

--- app/Model/Admin/Subcategory.php
+++ app/Model/Admin/Subcategory.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategory extends Model
 {
     protected $fillable = [
-        'subcategory_name', 'category_id',
+        'category_id',
+        'subcategory_name', 
     ];
 }
