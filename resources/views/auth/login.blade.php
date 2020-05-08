705e20594dcdf46491f60ca92954ef6bc617facd
Irfan-Chowdhury <irfanchowdhury80@gmail.com>
2020-05-08 12:39:37 +0600

    Front - Login/Register Page Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,79 +1,78 @@
 @extends('layouts.app')
-
 @section('content')
-<!-- Contact Form -->
-<link rel="stylesheet" type="text/css" href="{{asset('public/frontend/styles/contact_styles.css')}}">  
+
+<link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/styles/contact_styles.css')}}">
     <div class="contact_form">
         <div class="container">
             <div class="row">
-    <div class="col-lg-5 offset-lg-1" style="border:1px solid grey; padding: 20px;">
-        <div class="contact_form_container">
-            <div class="contact_form_title text-center">Sing In</div>
+                <div class="col-lg-5 offset-lg-1"  style="border: 1px solid grey; padding: 20px;">
+                    <div class="contact_form_container">
+                        <div class="contact_form_title text-center"><h2>Sign In</h2></div>
 
-            <form action="{{route('login')}}" method="post">
-              @csrf
-             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  placeholder="Enter email">
-              </div> 
-  
-                  <div class="form-group">
-                <label for="exampleInputEmail1">Password</label>
-                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}"  placeholder="Enter password">
-            
-              </div> 
-       
-                <div class="contact_form_button">
-                    <button type="submit" class="btn btn-info">Login</button>
-                </div><br>
-                <a href="">I Forgot Password</a>
-            </form>
-            <br><br><br>
-              <button type="submit"  class="btn btn-primary btn-block">Facebook</button>
-              <a href="{{ url('/auth/redirect/google') }}"  class="btn btn-danger btn-block">Google</a>
-        </div>
-    </div>
-
-     <div class="col-lg-5 offset-lg-1" style="border:1px solid grey; padding: 20px;">
-        <div class="contact_form_container">
-            <div class="contact_form_title text-center">Sign Up</div>
-
-            <form action="{{route('register')}}" method="post">
-              @csrf
-
-              <div class="form-group">
-                <label>Full Name</label>
-                <input type="text" class="form-control" name="name" placeholder="Enter name">
-              </div> 
-               <div class="form-group">
-                <label>Email address</label>
-                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Enter email">
-              </div> 
-
-               <div class="form-group">
-                <label>Phone</label>
-                <input type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" placeholder="Enter Number">
-              </div>
-
-              <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control" name="password"  placeholder="Enter email">
-            
-              </div> 
-              <div class="form-group">
-                <label >Confirm Password</label>
-                <input type="password" class="form-control" name="password_confirmation" placeholder="Enter Confirm password">
-            
-              </div> 
-                
-                <div class="contact_form_button">
-                    <button type="submit" class="button contact_submit_button">Sign up</button>
+                        <form action="{{ route('login') }}" id="contact_form" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email Or Phone</label>
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  aria-describedby="emailHelp" placeholder="Email Or Phone" required="">
+                                @error('email')
+                                   <span class="invalid-feedback" role="alert">
+                                       <strong>{{ $message }}</strong>
+                                   </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Password</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required=""  aria-describedby="emailHelp" placeholder="Password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="contact_form_button">
+                                <button type="submit" class="btn btn-success">Login</button>
+                            </div>
+                        </form><br>
+                        <a href="{{ route('password.request') }}">I Forgot My Password.</a>
+                        <br><br>
+                        <button type="submit" class="btn btn-primary btn-block">Login With Facebook</button>
+                        <a href="{{ url('/auth/redirect/google') }}" class="btn btn-danger btn-block">Login With Google</a>
+                    </div>
                 </div>
-            </form>
+                 <div class="col-lg-5 offset-lg-1" style="border: 1px solid grey; padding: 20px;">
+                    <div class="contact_form_container">
+                        <div class="contact_form_title text-center"><h2>Sign Up</h2></div>
 
-        </div>
-    </div>
-</div>
+                        <form action="{{ route('register') }}" id="contact_form" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Full Name </label>
+                                <input type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Full Name " name="name" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Phone </label>
+                                <input type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}"  aria-describedby="emailHelp" placeholder="Phone "  required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email </label>
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  aria-describedby="emailHelp" placeholder="Email " required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Password</label>
+                                <input type="password" class="form-control"  aria-describedby="emailHelp" placeholder="Password" name="password" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Confirm Password</label>
+                                <input type="password" class="form-control"  aria-describedby="emailHelp" placeholder="Re-type Password" name="password_confirmation" required="">
+                            </div>
+                            <div class="contact_form_button">
+                                <button type="submit" class="btn btn-info">SIgnUp</button>
+                            </div>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="panel"></div>
     </div>
